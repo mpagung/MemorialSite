1b707527b9d1477832b66aac72ee276871492950
mpagung <michaelputra.agung@gmail.com>
2021-04-09 10:51:56 +0700

    refactoring

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express"),
   app = express(),
   port = process.env.PORT || 5000,
-  cors = require("cors");
+  cors = require("cors"),
+  bodyParser = require("body-parser"),
+  https = require("https"),
+  dotenv = require('dotenv'),
+  subscribeModule = require('./public/module/subscribeModule.js');
 
-const bodyParser = require("body-parser");
-const https = require("https");
-const dotenv = require('dotenv');
 dotenv.config();
 
 app.set("view_engine", "ejs")
@@ -43,55 +44,8 @@ app.get("/subscriptionSuccess", (req, res) => {
 });
 
 app.post("/subscribe", (req, res) => {
-  var fullName=req.body.inputName;
-  var email=req.body.inputEmail;
-  var weekReminder=req.body.week;
-  var threeDReminder=req.body.threeD;
-  var oneDReminder=req.body.oneD;
-  var zeroDReminder=req.body.zeroD;
-  var tags=[];
-  if (weekReminder){
-    tags.push("WeekReminder")
-  }
-  if (threeDReminder){
-    tags.push("threeDaysReminder")
-  }
-  if (oneDReminder){
-    tags.push("oneDayReminder")
-  }
-  if (zeroDReminder){
-    tags.push("zeroDayReminder")
-  }
-
-  var data={
-    members: [
-      {
-        email_address:email,
-        status:"subscribed",
-        merge_fields: {
-          FULLNAME:fullName
-        },
-        tags : tags
-      }
-    ]};
-
-  const jsonData=JSON.stringify(data);
-
-  const api_key=process.env.MAILCHIMP_KEY;
-  const list_id=process.env.LIST_ID;
-  const url = "https://"+"us7"+".api.mailchimp.com/3.0/lists/"+list_id;
-  const options = {
-    method : "POST",
-    auth: "mpagung:"+api_key
-  }
-  const request = https.request(url,options,function(response){
-    response.on("data", function(data){
-      // console.log(JSON.parse(data))
-    })
-  })
-
-  request.write(jsonData);
-  request.end();
+  const body=req.body;
+  subscribeModule(req,res,body);
   res.redirect("/subscriptionSuccess");
 
 });
